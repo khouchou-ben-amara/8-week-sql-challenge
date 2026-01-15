1473a8c2f4b18ad342ec8929654c2b02ec31307e
khouchou-ben-amara <benamara.khouchou@gmail.com>
2026-01-15 16:48:56 +0100

    Delete case-study-1-dannys-diner directory

--- case-study-1-dannys-diner/case-study-1-dannys-diner/case-study-1-dannys-diner/insights.md
+++
@@ -1,6 +0,0 @@
-# Insights – Danny’s Diner
-
-This file contains business insights derived from the SQL queries.
-
-## Question 1
-(Insight to be added after analysis)
--- case-study-1-dannys-diner/case-study-1-dannys-diner/solutions.sql
+++
@@ -1,6 +0,0 @@
--- Case Study 1: Danny's Diner
--- Author: Khouchou Ben Amara
--- This file contains my SQL solutions to the case study questions.
-
--- Question 1
--- What is the total amount each customer spent at the restaurant?
